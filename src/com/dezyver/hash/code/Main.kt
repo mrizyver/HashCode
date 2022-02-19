1bba18abc82c07c4ff867f1d2a7f3b0239d839e7
Bogdan Onischenko <bogdan.onischenko@nixsolutions.com>
2022-02-19 17:59:56 +0200

    Changed smth

--- src/com/dezyver/hash/code/Main.kt
+++ src/com/dezyver/hash/code/Main.kt
@@ -1,21 +1,71 @@
 package com.dezyver.hash.code
 
 import java.io.File
+import java.lang.NullPointerException
+import java.util.*
 import kotlin.collections.ArrayList
 import kotlin.collections.HashSet
+import kotlin.random.Random
 
 const val basic = "input/b_basic.in.txt"
 const val coarse = "input/c_coarse.in.txt"
 const val difficult = "input/d_difficult.in.txt"
 const val elaborate = "input/e_elaborate.in.txt"
 
-fun main() {
-    val input: List<Wish> = parseInput(File(difficult))
-    val groupedWishes = groupWishes(input)
-    println(calculateScore(input, groupedWishes.first().likes))
+data class Wish(val likes: Set<String>, val dislikes: Set<String>)
+
+class Input(val list: List<Wish>) : Iterable<Wish>{
+    override fun iterator(): Iterator<Wish> {
+        return list.iterator()
+    }
 }
 
-data class Wish(val likes: Set<String>, val dislikes: Set<String>)
+typealias Solution = Set<String>
+
+class EstimatedSolution(val score: Int, val solution: Solution)
+
+fun main() {
+    val input = Input(parseInput(File(difficult)))
+
+    var generation = makeInitialGeneration(input)
+
+    repeat (1000) {
+        // 1 estimate current generation
+        val estimated: List<EstimatedSolution> = generation.map { sol -> EstimatedSolution(calculateScore(input.list, sol), sol) }
+
+        // 2 find the best solution, log generation number, max score of this generation
+        val best: EstimatedSolution = estimated.maxByOrNull { it.score } ?: return@repeat
+
+        println("Generation $it, max score ${best.score}")
+
+        // 3 run selection
+        val selected = selection(estimated)
+
+        // 4 run mutation
+        generation = mutate(selected)
+    }
+}
+
+fun makeInitialGeneration (input: Input): List<Solution> {
+    val times = 100
+    val solution = ArrayList<Set<String>>(times)
+    repeat(times) {
+        val element = input.fold(HashSet()) { result: Set<String>, wish: Wish ->
+            if (Random.nextInt() % 50 != 0) result
+            else result + wish.likes + wish.dislikes
+        }
+        solution.add(element)
+    }
+    return solution
+}
+
+fun selection (solutions: List<EstimatedSolution>): List<EstimatedSolution> {
+    return listOf()
+}
+
+fun mutate (solutions: List<EstimatedSolution>): List<Solution> {
+    return listOf()
+}
 
 fun groupWishes(wishes: List<Wish>): List<Wish> {
     val sortedWishes = wishes.sortedWith { o1, o2 -> (o1.dislikes.size - o1.likes.size) - (o2.dislikes.size - o2.likes.size) }

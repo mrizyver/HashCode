fee67acba4604868f04dae5d1bc423c39b9aa96d
izyver <mr.izyver@gmail.com>
2022-02-23 22:16:53 +0200

    Added average

--- src/com/dezyver/hash/code/OnePizza.kt
+++ src/com/dezyver/hash/code/OnePizza.kt
@@ -13,7 +13,7 @@ const val elaborate = "input/e_elaborate.in.txt"
 
 data class Wish(val likes: Set<String>, val dislikes: Set<String>)
 
-class Input(val list: List<Wish>, val ingridients: Set<String>) : Iterable<Wish>{
+class Input(val list: List<Wish>, val ingridients: Set<String>) : Iterable<Wish> {
     override fun iterator(): Iterator<Wish> {
         return list.iterator()
     }
@@ -33,16 +33,23 @@ fun main() {
     val input = Input(inputList, ingredients)
     val availableProcessors = Runtime.getRuntime().availableProcessors()
     val generations = ArrayBlockingQueue<List<Solution>>(availableProcessors * 2, false)
-    generations.offer(listOf(bogdan(inputList)))
+    generations.offer(groupWishes(inputList).map { it.likes })
     val estimates = ArrayBlockingQueue<List<EstimatedSolution>>(availableProcessors * 2, false)
     var gen = 0
     var totalBest: EstimatedSolution? = null
-    repeat(availableProcessors - 1){
+    repeat(availableProcessors - 1) {
         thread {
             while (true) {
                 // 1 estimate current generation
                 val generation: List<Solution> = generations.take()
-                estimates.offer(generation.map { s -> EstimatedSolution(calculateScore(input.list, s), s) })
+                estimates.offer(generation.map { s ->
+                    EstimatedSolution(
+                        calculateScore(
+                            input.list,
+                            s
+                        ), s
+                    )
+                })
             }
         }
     }
@@ -50,11 +57,18 @@ fun main() {
         val estimated = estimates.take()
         // 2 find the best solution, log generation number, max score of this generation
         val best: EstimatedSolution = estimated.maxByOrNull { it.score } ?: continue
-        if (totalBest?.score ?: 0 < best.score){
+        var average: Float? = null
+        if (average == null) {
+            average = best.score.toFloat()
+        } else {
+            average += best.score.toFloat()
+            average /= 2f
+        }
+        if (totalBest?.score ?: 0 < best.score) {
             totalBest = best
             best.isBest = true
         }
-        println("Generation ${gen++}, max score ${best.score}, ingrs ${best.solution.size}, the best ${totalBest?.score}, generations ${generations.size}, estimates ${estimates.size}")
+        println("Generation ${gen++}, max score ${best.score}, best score ${totalBest?.score}, average score $average, ingrs ${best.solution.size}, generations ${generations.size}, estimates ${estimates.size}")
         // 3 run selection
         val selected = selection(estimated)
         // 4 run mutation
@@ -65,7 +79,7 @@ fun main() {
     }
 }
 
-fun makeInitialGeneration (input: Input): List<Solution> {
+fun makeInitialGeneration(input: Input): List<Solution> {
     val times = 100
     val solution = ArrayList<Set<String>>(times)
     repeat(times) {
@@ -78,19 +92,20 @@ fun makeInitialGeneration (input: Input): List<Solution> {
     return solution
 }
 
-val genSize = 50;
+val genSize = 50
 
-fun selection (solutions: List<EstimatedSolution>): List<EstimatedSolution> {
+fun selection(solutions: List<EstimatedSolution>): List<EstimatedSolution> {
     val bestScore = solutions.maxOf { it.score }
     val worstScore = solutions.minOf { it.score }
     if (solutions.size < genSize) return solutions
     return solutions.filter {
-        val probability = (it.score.toFloat() - worstScore) / (bestScore.toFloat() - worstScore) + 0.1f
+        val probability =
+            (it.score.toFloat() - worstScore) / (bestScore.toFloat() - worstScore) + 0.1f
         Random.nextFloat() < probability
     }
 }
 
-fun mutate (input: Input, solutions: List<EstimatedSolution>): List<Solution> {
+fun mutate(input: Input, solutions: List<EstimatedSolution>): List<Solution> {
     val mutations = LinkedList<Solution>()
     for (solution: EstimatedSolution in solutions) {
         if (solution.isBest) {
@@ -99,7 +114,7 @@ fun mutate (input: Input, solutions: List<EstimatedSolution>): List<Solution> {
 
         if (solutions.size < genSize) {
             val temp = solution.solution.toMutableSet()
-            if (Random.nextBoolean() && temp.size > 1){
+            if (Random.nextBoolean() && temp.size > 1) {
                 temp.remove(temp.random())
             } else {
                 temp.add(input.ingridients.random())
@@ -115,14 +130,18 @@ fun mutate (input: Input, solutions: List<EstimatedSolution>): List<Solution> {
 }
 
 fun groupWishes(wishes: List<Wish>): List<Wish> {
-    val sortedWishes = wishes.sortedWith { o1, o2 -> (o1.dislikes.size - o1.likes.size) - (o2.dislikes.size - o2.likes.size) }
+    val sortedWishes =
+        wishes.sortedWith { o1, o2 -> (o1.dislikes.size - o1.likes.size) - (o2.dislikes.size - o2.likes.size) }
     val groupedWishes = ArrayList<Wish>()
     client@ for (wish in sortedWishes) {
         group@ for ((index, group) in groupedWishes.withIndex()) {
-            val likesConflict: Boolean = wish.likes.fold(false) { acc, str -> group.dislikes.contains(str) || acc }
-            val dislikesConflict: Boolean = wish.dislikes.fold(false) { acc, str -> group.likes.contains(str) || acc }
-            if (!likesConflict && !dislikesConflict){
-                groupedWishes[index] = Wish(group.likes + wish.likes, group.dislikes + wish.dislikes)
+            val likesConflict: Boolean =
+                wish.likes.fold(false) { acc, str -> group.dislikes.contains(str) || acc }
+            val dislikesConflict: Boolean =
+                wish.dislikes.fold(false) { acc, str -> group.likes.contains(str) || acc }
+            if (!likesConflict && !dislikesConflict) {
+                groupedWishes[index] =
+                    Wish(group.likes + wish.likes, group.dislikes + wish.dislikes)
                 continue@client
             }
         }
@@ -139,23 +158,30 @@ fun parseInput(file: File): List<Wish> {
     while (num * 2 > index) {
         val liked = lines[++index].split(" ")
         val disliked = lines[++index].split(" ")
-        output.add(Wish(liked.subList(1, liked.size).toSet(), disliked.subList(1, disliked.size).toSet()))
+        output.add(
+            Wish(
+                liked.subList(1, liked.size).toSet(),
+                disliked.subList(1, disliked.size).toSet()
+            )
+        )
     }
     return output
 }
 
-fun calculateScore (wishes: Collection<Wish>, recipe: Set<String>): Int {
+fun calculateScore(wishes: Collection<Wish>, recipe: Set<String>): Int {
     return wishes.fold(0) { score, wish ->
-        val wishFits = recipe.containsAll(wish.likes) && !wish.dislikes.fold(false) { b, dis -> recipe.contains(dis) || b }
+        val wishFits = recipe.containsAll(wish.likes) && !wish.dislikes.fold(false) { b, dis ->
+            recipe.contains(dis) || b
+        }
         if (wishFits) score + 1 else score
     }
 }
 
 fun bogdan(wishes: List<Wish>): Solution {
     var maxScore = 0
-    var bestRecipe =  HashSet<String>()
+    var bestRecipe = HashSet<String>()
 
-    fun checkBest (recipe: HashSet<String>) {
+    fun checkBest(recipe: HashSet<String>) {
         val score = calculateScore(wishes, recipe)
         if (score > maxScore) {
             maxScore = score
@@ -165,10 +191,10 @@ fun bogdan(wishes: List<Wish>): Solution {
 
     for (wish in wishes) {
         val needToAdd = wish.likes.filter { like -> !bestRecipe.contains(like) }
-        val needToRemove = wish.dislikes.filter{like -> bestRecipe.contains(like) }
+        val needToRemove = wish.dislikes.filter { like -> bestRecipe.contains(like) }
         val recipe = HashSet<String>(bestRecipe)
-        needToAdd.forEach{item -> recipe.add(item)}
-        needToRemove.forEach{item -> recipe.remove(item)}
+        needToAdd.forEach { item -> recipe.add(item) }
+        needToRemove.forEach { item -> recipe.remove(item) }
         checkBest(recipe)
     }
     return bestRecipe
